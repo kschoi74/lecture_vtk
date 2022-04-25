2ec48120cecc48499a4daff3d335365e8ece80c7
Kang-Sun Choi <kangsun.choi@gmail.com>
2022-04-25 14:06:23 +0900

    ConeSource 완료

---
+++ MFCDlgEx/MFCDlgExDlg.cpp
@@ -0,0 +1,217 @@
+﻿
+// MFCDlgExDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCDlgEx.h"
+#include "MFCDlgExDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCDlgExDlg 대화 상자
+
+
+
+CMFCDlgExDlg::CMFCDlgExDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCDLGEX_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCDlgExDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCDlgExDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_CONE, &CMFCDlgExDlg::OnBnClickedButtonCone)
+END_MESSAGE_MAP()
+
+
+// CMFCDlgExDlg 메시지 처리기
+
+BOOL CMFCDlgExDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	if (this->GetDlgItem(IDC_STATIC_FRAME))
+	{
+		this->InitVtkWindow(this->GetDlgItem(IDC_STATIC_FRAME)->GetSafeHwnd());
+		this->ResizeVtkWindow();
+	}
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCDlgExDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCDlgExDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCDlgExDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCDlgExDlg::OnBnClickedButtonCone()
+{
+	vtkSmartPointer<vtkConeSource> coneSource =
+		vtkSmartPointer<vtkConeSource>::New();
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(coneSource->GetOutputPort());
+	vtkSmartPointer<vtkActor> actor =
+		vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+	renderer->SetBackground(.1, .2, .3);
+	renderer->ResetCamera();
+
+	m_vtkWindow->AddRenderer(renderer);
+	m_vtkWindow->Render();
+}
+
+void CMFCDlgExDlg::InitVtkWindow(void* hWnd)
+{
+	if (m_vtkWindow == NULL)
+	{
+		vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+			vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+		interactor->SetInteractorStyle(
+			vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New());
+
+		vtkSmartPointer<vtkRenderer> renderer =
+			vtkSmartPointer<vtkRenderer>::New();
+		renderer->SetBackground(0.0, 0.0, 0.0);
+
+		m_vtkWindow = vtkSmartPointer<vtkRenderWindow>::New();
+		m_vtkWindow->SetParentId(hWnd);
+		m_vtkWindow->SetInteractor(interactor);
+		m_vtkWindow->AddRenderer(renderer);
+		m_vtkWindow->Render();
+	}
+}
+
+void CMFCDlgExDlg::ResizeVtkWindow()
+{
+	CRect rc;
+	CWnd* pWnd = GetDlgItem(IDC_STATIC_FRAME);
+	if (pWnd)
+	{
+		pWnd->GetClientRect(rc);
+		m_vtkWindow->SetSize(rc.Width(), rc.Height());
+	}
+}
---
+++ MFCDlgEx/MFCDlgExDlg.h
@@ -0,0 +1,40 @@
+﻿
+// MFCDlgExDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCDlgExDlg 대화 상자
+class CMFCDlgExDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCDlgExDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	vtkSmartPointer<vtkRenderWindow> m_vtkWindow;
+	void InitVtkWindow(void* hWnd);
+	void ResizeVtkWindow();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCDLGEX_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonCone();
+};
